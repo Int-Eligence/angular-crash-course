b7e6835280706d4928364cb716403337589681fa
Int-elligence <soumyaidmail@gmail.com>
2021-10-24 11:57:27 +0530

    Components

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+  @Input() text: string = "Click!";
+  @Input() color: string = "Black";
+  @Output() btnClicked = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onclickBtn(): void {
+    this.btnClicked.emit();
+  }
+
+}
